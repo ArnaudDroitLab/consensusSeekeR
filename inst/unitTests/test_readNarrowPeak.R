5850d4ec8bc6dafb168c40bd6ffc11f0608ba7df
adeschen <adeschen@hotmail.com>
2015-05-11 14:38:07 -0400

    Adding first unit tests for readNarrowPeak() function

---
+++ inst/unitTests/test_readNarrowPeak.R
@@ -0,0 +1,27 @@
+###################################################
+# Created by Astrid Louise Deschenes
+# 2015-05-11
+###################################################
+
+###################################################
+## Test the readnarrowPeak() function
+###################################################
+
+### {{{ --- Test setup ---
+
+if(FALSE) {
+    library( "RUnit" )
+    library( "sharedBed" )
+}
+
+### }}}
+
+## Test the result when the file doesn't exist
+test.readNarrowPeak_no_existing_file <- function() {
+    file <- "TiiTooTiiToo.narrowPeak"
+    obs <- tryCatch(readNarrowPeak(file_path = file), 
+                    error=conditionMessage)
+    exp <- paste0("No such file \"", file, "\"")
+    message <- paste0("readNarrowPeak_no_existing_file() ",
+        "- A not existing file did not generated the expected exception.")
+    checkEquals(obs, exp, msg = message)
