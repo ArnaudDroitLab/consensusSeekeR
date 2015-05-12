3325a013b6c40aee57331c6679c1fae747b0d650
adeschen <adeschen@hotmail.com>
2015-05-12 16:39:50 -0400

    Changing package description

--- R/sharedBed.R
+++ R/sharedBed.R
@@ -1,9 +1,10 @@
-#' sharedBed: Detection of shared features inside a group of experiences using 
-#' BED file
+#' sharedBed: Detection of consensus peak regions inside a group of experiences
+#' using narrowPeak files
 #' 
-#' This package compares multiple BED files to extract common 
-#' features. The size of the analyzed region is adjustable as well as the
-#' number of BED files in which the features are detected to be retained. 
+#' This package compares multiple narrowPeak files to extract common 
+#' consensus peak regions. The size of the analyzed region is adjustable 
+#' as well as the number of BED files in which the features are detected to 
+#' be retained. 
 #' 
 #' @docType package
 #' @name sharedBed-package
