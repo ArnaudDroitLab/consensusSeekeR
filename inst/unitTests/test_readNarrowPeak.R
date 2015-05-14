f6da03bb52e2f7300723a6d8215d56657a16f278
adeschen <adeschen@hotmail.com>
2015-05-14 17:10:34 -0400

    Adding unit tests for readNarrowPeak(). Adding demo files used by unit tests

--- inst/unitTests/test_readNarrowPeak.R
+++ inst/unitTests/test_readNarrowPeak.R
@@ -16,6 +16,11 @@ if(FALSE) {
 
 ### }}}
 
+file_FOSL2_Rep01 <- dir(system.file("extdata", package = "sharedBed"), 
+            pattern = "Hosa_A549_FOSL2_ENCSR000BQO_ENCFF000MZT_peaks_part_chr1_and_chr12.narrowPeak$",
+            full.names=TRUE)
+
+
 ## Test the result when the file doesn't exist
 test.readNarrowPeak_no_existing_file <- function() {
     file <- "TiiTooTiiToo.narrowPeak"
@@ -24,5 +29,44 @@ test.readNarrowPeak_no_existing_file <- function() {
     exp <- paste0("No such file \"", file, "\"")
     message <- paste0("readNarrowPeak_no_existing_file() ",
         "- A not existing file did not generated the expected exception.")
+    checkEquals(obs, exp, msg = message)
+}
+
+## Test the result when extractPeaks and extractRegions parameters
+## are both FALSE
+test.readNarrowPeak_both_parameters_FALSE <- function() {
+    obs <- tryCatch(readNarrowPeak(file_path = file_FOSL2_Rep01, 
+                    extractRegions = FALSE, extractPeaks = FALSE), 
+                    error=conditionMessage)
+    exp <- "extractPeaks and extractRegions cannot be both FALSE"
+    message <- paste0("readNarrowPeak_no_existing_file() ",
+                     "- Both extractPeaks and extractRegions set to ",
+                    "FALSE did not generated the expected exception.")
+    checkEquals(obs, exp, msg = message)
+}
+
+## Test the result when extractPeaks is a string
+test.readNarrowPeak_a_string_as_extractPeaks <- function() {
+    obs <- tryCatch(readNarrowPeak(file_path = file_FOSL2_Rep01, 
+                                    extractRegions = FALSE, 
+                                    extractPeaks = "extremeYoga"), 
+                                    error=conditionMessage)
+    exp <- "extractPeaks must be a logical value"
+    message <- paste0("readNarrowPeak_no_existing_file() ",
+                      "- A string used as extractPeaks parameter ",
+                      "did not generated the expected exception.")
+    checkEquals(obs, exp, msg = message)
+}
+
+## Test the result when extractRegions is a string
+test.readNarrowPeak_a_string_as_extractRegions <- function() {
+    obs <- tryCatch(readNarrowPeak(file_path = file_FOSL2_Rep01, 
+                                   extractRegions = "extremeReading", 
+                                   extractPeaks = TRUE), 
+                    error=conditionMessage)
+    exp <- "extractRegions must be a logical value"
+    message <- paste0("readNarrowPeak_no_existing_file() ",
+                      "- A string used as extractRegions parameter ",
+                      "did not generated the expected exception.")
     checkEquals(obs, exp, msg = message)
 }
