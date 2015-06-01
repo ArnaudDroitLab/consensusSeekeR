cf75e2358adfd6e2b76749c2332f12fae7441888
adeschen <adeschen@hotmail.com>
2015-06-01 18:11:46 -0400

    Adding unit test for intern functions. Adding extern data files

---
+++ inst/unitTests/test_findConsensusPeakRegionsIntern.R
@@ -0,0 +1,67 @@
+###################################################
+# Created by Astrid Louise Deschenes
+# 2015-06-01
+###################################################
+
+###################################################
+## Test the findConsensusPeakRegionsIntern.R functions
+###################################################
+
+### {{{ --- Test setup ---
+
+if(FALSE) {
+    library( "RUnit" )
+    library( "sharedBed" )
+}
+
+
+### }}}
+
+data(Hosa_A549_FOSL2_01_Peaks)
+data(Hosa_A549_FOSL2_01_NarrowPeaks)
+data(Hosa_A549_FOXA1_01_Peaks)
+data(Hosa_A549_FOXA1_01_NarrowPeaks)
+data(Hosa_A549_FOXA2_01_Peaks)
+data(Hosa_A549_FOXA2_01_NarrowPeaks)
+
+
+###################################################
+## Test the isInteger function
+###################################################
+
+## Test the result when a integer is passed to the function
+test.isInteger_with_integer <- function() {
+    obs <- tryCatch(sharedBed:::isInteger(value = 444), 
+                    error=conditionMessage)
+    message <- paste0("isInteger_with_integer() ",
+                    "- A integer did not generated TRUE.")
+    checkEquals(obs, TRUE, msg = message)
+}
+
+## Test the result when a string is passed to the function
+test.isInteger_with_string <- function() {
+    obs <- tryCatch(sharedBed:::isInteger(value = "444"), 
+                    error=conditionMessage)
+    message <- paste0("isInteger_with_integer() ",
+                    "- A string did not generated FALSE.")
+    checkEquals(obs, FALSE, msg = message)
+}
+
+## Test the result when a list is passed to the function
+test.isInteger_with_list_of_integers <- function() {
+    obs <- tryCatch(sharedBed:::isInteger(value = list(a=444, b=333)), 
+                    error=conditionMessage)
+    message <- paste0("isInteger_with_integer() ",
+                    "- A list did not generated FALSE.")
+    checkEquals(obs, FALSE, msg = message)
+}
+
+## Test the result when a vector is passed to the function
+test.isInteger_with_vector_of_integers <- function() {
+    obs <- tryCatch(sharedBed:::isInteger(value = c(444,333)), 
+                    error=conditionMessage)
+    message <- paste0("isInteger_with_integer() ",
+                "- A vector of integers did not generated FALSE.")
+    checkEquals(obs, FALSE, msg = message)
+}
+
