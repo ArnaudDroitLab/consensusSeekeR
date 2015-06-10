e01dc7086970e94bdf13a5069e59e1c348614563
adeschen <adeschen@hotmail.com>
2015-06-10 13:14:36 -0400

    Adding description to dataset

--- R/consensusSeekeR.R
+++ R/consensusSeekeR.R
@@ -1,19 +1,19 @@
-#' consensusSeekeR: Detection of consensus peak regions inside a group of 
+#' consensusSeekeR: Detection of consensus peak regions inside a group of
 #' experiments using narrowPeak files
-#' 
-#' This package compares multiple narrowPeak files to extract common 
-#' consensus peak regions. The size of the analyzed region is adjustable 
-#' as well as the number of narrowPeak files in which a peak must be detected 
-#' in the potential region to mark the region as a consensus peak region. 
-#' 
+#'
+#' This package compares multiple narrowPeak files to extract common
+#' consensus peak regions. The size of the analyzed region is adjustable
+#' as well as the number of narrowPeak files in which a peak must be detected
+#' in the potential region to mark the region as a consensus peak region.
+#'
 #' @docType package
 #' @name consensusSeekeR-package
 #' @aliases consensusSeekeR-package consensusSeekeR
 #' @author  Astrid Louise Deschenes,
 #'  Fabien Claude Lamaze,
-#'  Pascal Belleau and 
+#'  Pascal Belleau and
 #'  Arnaud Droit
-#' 
+#'
 #' Maintainer: Astrid Louise Deschenes <astrid-louise.deschenes@@crchudequebec.ulaval.ca>
 #' @keywords package
 NULL
@@ -21,17 +21,22 @@ NULL
 #' TODO Data Set
 #'
 #' TODO
-#' 
+#'
 #' @name Hosa_A549_FOSL2_01_NarrowPeaks
 #' @docType data
-#' @format A \code{GRanges} 
+#' @format A \code{GRanges}
 #' @keywords datasets
 NULL
 
 #' TODO Data Set
 #'
-#' TODO
-#' 
+#' Sites with the greatest evidence of transcription factor binding for the
+#' FOSL2 transcription factor (DCC accession: ENCFF000MZT), calculated
+#' using the MACS peak caller (Zhang et al., 2008),
+#' for regions chr1:249120200-249250621 and chr10:1-370100
+#' from
+#' the Encyclopedia of DNA Elements (ENCODE) data (Dunham I et al. 2012).
+#'
 #' @name Hosa_A549_FOSL2_01_Peaks
 #' @docType data
 #' @format A \code{GRanges}
@@ -42,42 +47,68 @@ NULL
 #' TODO Data Set
 #'
 #' TODO
-#' 
+#'
 #' @name Hosa_A549_FOSL2_01_NarrowPeaks_partial
 #' @docType data
-#' @format A \code{GRanges} 
+#' @format A \code{GRanges}
+#' @references
+#'  \itemize{
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
+#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
+#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
+#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  }
 #' @keywords datasets
 NULL
 
 #' TODO Data Set
 #'
 #' TODO
-#' 
+#'
 #' @name Hosa_A549_FOSL2_01_Peaks_partial
 #' @docType data
 #' @format A \code{GRanges}
 #' @usage data(Hosa_A549_FOSL2_01_Peaks_partial)
+#' @references
+#'  \itemize{
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
+#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
+#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
+#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  }
 #' @keywords datasets
 NULL
 
 #' TODO Data Set
 #'
 #' TODO
-#' 
+#'
 #' @name Hosa_A549_FOXA1_01_NarrowPeaks_partial
 #' @docType data
-#' @format A \code{GRanges} 
+#' @format A \code{GRanges}
 #' @usage data(Hosa_A549_FOXA1_01_NarrowPeaks_partial)
+#' @references
+#'  \itemize{
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
+#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
+#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
+#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  }
 #' @keywords datasets
 NULL
 
 #' TODO Data Set
 #'
-#' TODO
-#' 
 #' @name Hosa_A549_FOXA1_01_Peaks_partial
 #' @docType data
 #' @format A \code{GRanges}
 #' @usage data(Hosa_A549_FOXA1_01_Peaks_partial)
+#' @references
+#'  \itemize{
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
+#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
+#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
+#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  }
 #' @keywords datasets
 NULL
