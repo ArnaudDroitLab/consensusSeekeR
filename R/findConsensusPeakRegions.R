bb92b43eac4bea049b2cca3304b3d1b8d125f9b1
adeschen <adeschen@hotmail.com>
2015-06-10 10:22:24 -0400

    Changing assignation. Formatting code

--- R/findConsensusPeakRegions.R
+++ R/findConsensusPeakRegions.R
@@ -1,108 +1,108 @@
 #' @title Extract regions sharing the same features in more than one experiment
-#' 
+#'
 #' @description Find regions sharing the same features for a minimum number of
-#'          experiments using called peaks of signal enrichment based on 
-#'          pooled, normalized data (mainly coming from narrowPeak files). 
-#'          The peaks and narrow peaks and used to identify 
+#'          experiments using called peaks of signal enrichment based on
+#'          pooled, normalized data (mainly coming from narrowPeak files).
+#'          The peaks and narrow peaks and used to identify
 #'          the common regions. The minimum number of experiments that must a
-#'          peak in a common regions for that region to be selected is 
-#'          specified by user, as well as the size of padding. Only the 
-#'          chromosomes specified by the user are treated. The function can be 
-#'          parallized by specifying a number of threads superior to 1. 
-#'          
-#'          When the padding is small, the detected regions are smaller than 
+#'          peak in a common regions for that region to be selected is
+#'          specified by user, as well as the size of padding. Only the
+#'          chromosomes specified by the user are treated. The function can be
+#'          parallized by specifying a number of threads superior to 1.
+#'
+#'          When the padding is small, the detected regions are smaller than
 #'          the one that could be obtained by doing an overlap of the narrow
-#'          regions. Even more, the parameter specifying the minimum number of 
-#'          experiments needed to retain a region add versatility to the 
+#'          regions. Even more, the parameter specifying the minimum number of
+#'          experiments needed to retain a region add versatility to the
 #'          function.
-#'          
+#'
 #'          The side of the padding can have a large effect on the detected
 #'          regions. It is recommanded to test more than one size and to do
 #'          some manual validation of the resulting regions before selecting
 #'          the final padding size.
-#' 
-#' @param narrowPeaks a \code{vector} containing \code{GRanges} representing 
-#'          called peaks of signal enrichment based on pooled, normalized data 
+#'
+#' @param narrowPeaks a \code{vector} containing \code{GRanges} representing
+#'          called peaks of signal enrichment based on pooled, normalized data
 #'          for all experiments.
 #' @param peaks a \code{vector} containing \code{GRanges} representing peaks.
-#' @param chrInfo a \code{Seqinfo} containing the name and the length of the 
+#' @param chrInfo a \code{Seqinfo} containing the name and the length of the
 #'          chromosomes to analyze.
-#' @param extendingSize a \code{numeric} value indicating the size of padding 
+#' @param extendingSize a \code{numeric} value indicating the size of padding
 #'          at each side of the peaks median position to create the consensus
 #'          region. The minimum size of the consensu region will be equal to
-#'          twice the value of the \code{extendingSize} parameter. The size of 
+#'          twice the value of the \code{extendingSize} parameter. The size of
 #'          the \code{extendingSize} must be a positive integer. Default = 250.
 #' @param includeAllPeakRegion a \code{logical} indicating if the region size,
-#'          which is set by the \code{extendingSize} parameter is extended to 
-#'          include all region of the peak closest to the peaks median 
+#'          which is set by the \code{extendingSize} parameter is extended to
+#'          include all region of the peak closest to the peaks median
 #'          position for each experiment. When two peaks are at equal distance
 #'          of the peaks median for one experiment, both peaks are used to
 #'          extend the final consensus region.
 #' @param minNbrExp a \code{numeric} indicating the minimum number of BED files
 #'          in which a peak must be present for a region to be retained. The
-#'          numeric must be a positive integer inferior or equal to the number 
+#'          numeric must be a positive integer inferior or equal to the number
 #'          of files present in the \code{narrowPeakFiles} parameter.
 #'          Default = 1.
 #' @param nbrThreads a \code{numeric} indicating the number of threads to use
-#'          in parallel. The \code{nbrThreads} must be a positive integer. 
+#'          in parallel. The \code{nbrThreads} must be a positive integer.
 #'          Default = 1.
-#' 
-#' @return an object of \code{class} "consensusRanges". 
-#' 
+#'
+#' @return an object of \code{class} "consensusRanges".
+#'
 #' @author Astrid Louise Deschenes
 #' @importFrom BiocGenerics start end
 #' @importFrom stringr str_split
 #' @importFrom IRanges IRanges unlist
 #' @importFrom GenomicRanges GRanges GRangesList .__T__split:base
-#' @importFrom BiocParallel bplapply MulticoreParam SerialParam 
+#' @importFrom BiocParallel bplapply MulticoreParam SerialParam
 #'                  multicoreWorkers bpmapply
 #' @importFrom GenomeInfoDb Seqinfo seqinfo seqnames
 #' @export
-findConsensusPeakRegions <- function(narrowPeaks, peaks, chrInfo, 
-                               extendingSize = 250, 
-                               includeAllPeakRegion = TRUE, minNbrExp = 1, 
+findConsensusPeakRegions <- function(narrowPeaks, peaks, chrInfo,
+                               extendingSize = 250,
+                               includeAllPeakRegion = TRUE, minNbrExp = 1,
                                nbrThreads = 1) {
     cl <- match.call()
-    
+
     # Parameters validation
-    findConsensusPeakRegionsValidation(narrowPeaks, peaks, chrInfo, 
+    findConsensusPeakRegionsValidation(narrowPeaks, peaks, chrInfo,
             extendingSize, includeAllPeakRegion, minNbrExp, nbrThreads)
-    
+
     # Select the type of object used for parallel processing
     coreParam <- MulticoreParam(workers = nbrThreads)
     if (nbrThreads == 1 || multicoreWorkers() == 1) {
         coreParam <- SerialParam()
     }
-    
+
     # Preparing data
-    narrowPeaksSplit <- GenomicRanges::split(narrowPeaks, 
+    narrowPeaksSplit <- GenomicRanges::split(narrowPeaks,
                                                 seqnames(narrowPeaks))
     peaksSplit <- GenomicRanges::split(peaks, seqnames(peaks))
     rm(peaks)
     rm(narrowPeaks)
-    selectedNarrowPeaksSplit <- narrowPeaksSplit[names(narrowPeaksSplit) %in% 
+    selectedNarrowPeaksSplit <- narrowPeaksSplit[names(narrowPeaksSplit) %in%
                                                 seqnames(chrInfo)]
-    selectedPeaksSplit <- peaksSplit[names(peaksSplit) %in% 
+    selectedPeaksSplit <- peaksSplit[names(peaksSplit) %in%
                                                 seqnames(chrInfo)]
     rm(narrowPeaksSplit)
     rm(peaksSplit)
-    
+
     # Running each chromosome on a separate thread
     results2 <- bpmapply(findConsensusPeakRegionsForOneChrom,
                         chrName = seqnames(chrInfo),
-                        allPeaks = selectedPeaksSplit, 
-                        allNarrowPeaks = selectedNarrowPeaksSplit, 
-                        MoreArgs = c(extendingSize = extendingSize, 
+                        allPeaks = selectedPeaksSplit,
+                        allNarrowPeaks = selectedNarrowPeaksSplit,
+                        MoreArgs = c(extendingSize = extendingSize,
                         includeAllPeakRegion =
                         includeAllPeakRegion, minNbrExp = minNbrExp,
                         chrList = chrInfo),
                         BPPARAM = coreParam)
-    
+
     # Creating result list
     z <- list(call = cl,
-                    consensusRanges = IRanges::unlist(GRangesList((results2)), 
+                    consensusRanges = IRanges::unlist(GRangesList((results2)),
                     recursive = TRUE, use.names = FALSE))
-    
+
     class(z)<-"consensusRanges"
 
     return(z)
--- R/readNarrowPeak.R
+++ R/readNarrowPeak.R
@@ -1,97 +1,97 @@
 #' @title Extract narrow regions and peaks from a narrrowPeak file
-#' 
+#'
 #' @description Read a narrowPeak file and extract the narrow regions and/or
-#'          the peaks, as specified by used. The narrowPeak file must fit the 
-#'          UCSC specifications. See 
+#'          the peaks, as specified by used. The narrowPeak file must fit the
+#'          UCSC specifications. See
 #'          \url{https://genome.ucsc.edu/FAQ/FAQformat.html#format12} for more
 #'          details. The file can have one or many header lines. However, the
 #'          total number of header lines must be inferior to 250 lines.
-#' 
+#'
 #' @param file_path the name of the file.
 #' @param extractRegions a \code{logical} indicating if the narrow regions must
-#'          be extracted. If \code{TRUE}, a \code{GRanges} containing the 
-#'          narrow regions will be returned. Otherwise, \code{NULL} is 
+#'          be extracted. If \code{TRUE}, a \code{GRanges} containing the
+#'          narrow regions will be returned. Otherwise, \code{NULL} is
 #'          returned. Default = \code{TRUE}.
 #' @param extractPeaks a \code{logical} indicating if the peaks must
 #'          be extracted. If \code{TRUE}, a \code{GRanges} containing the peaks
-#'          will be returned. Otherwise, \code{NULL} is 
+#'          will be returned. Otherwise, \code{NULL} is
 #'          returned. Default = \code{TRUE}.
-#' 
+#'
 #' @return a \code{list} containing 2 entries:
 #'      \itemize{
-#'          \item narrowPeak a {\code{GRanges}} containing 
-#'              the narrow regions extracted from the file. {\code{NULL}} when 
+#'          \item narrowPeak a {\code{GRanges}} containing
+#'              the narrow regions extracted from the file. {\code{NULL}} when
 #'              not needed by user.
-#'          \item peak a {\code{GRanges}} containing 
+#'          \item peak a {\code{GRanges}} containing
 #'              the peaks extracted from the file. {\code{NULL}} when not
 #'              needed by user.
 #'      }
-#' 
+#'
 #' @author Astrid Louise Deschenes
 #' @importFrom BiocGenerics start end
 #' @importFrom IRanges IRanges
 #' @importFrom GenomicRanges GRanges
 #' @importFrom S4Vectors Rle
 #' @export
-readNarrowPeak<- function(file_path, extractRegions = TRUE, 
+readNarrowPeak<- function(file_path, extractRegions = TRUE,
                                 extractPeaks = TRUE) {
     # Parameters validation
     if (!file.exists(file_path)) {
         stop("No such file \"", file_path, "\"")
     }
-    
+
     if (!is.logical(extractRegions)) {
         stop("extractRegions must be a logical value")
     }
-    
+
     if (!is.logical(extractPeaks)) {
         stop("extractPeaks must be a logical value")
     }
-    
+
     if (!extractRegions && !extractPeaks) {
         stop("extractPeaks and extractRegions cannot be both FALSE")
     }
-    
+
     # The file can have one or many lines of comments as header
     # Find the first line which respect the UCSC format
-    data_size = 250
-    data = scan(file_path, what = "character", nmax = data_size, 
-                 strip.white = TRUE, sep = "\n", quiet = TRUE)
-    
+    data_size <- 250
+    data <- scan(file_path, what = "character", nmax = data_size,
+                    strip.white = TRUE, sep = "\n", quiet = TRUE)
+
     grepRes <- grep(paste0("^\\S+(\\s+\\d+){2}\\s+\\S+\\s+\\d+\\s+[-\\+*\\.]",
-                "(\\s+[0-9\\.]+){3}\\s+\\d+"), data)
-    
+                        "(\\s+[0-9\\.]+){3}\\s+\\d+"), data)
+
     if (length(grepRes) == 0) {
-        stop("No valid chromosome detected within first ", data_size, 
+        stop("No valid chromosome detected within first ", data_size,
                 " lines of BED file \"", file_path , "\"")
     }
-    
-    skip_lines = min(grepRes) - 1
-    
+
+    skip_lines <- min(grepRes) - 1
+
     # Extract info from file and load it into a table
-    peaks = read.table(file_path, header = FALSE, skip = skip_lines)
-    peaks = peaks[,1:10];
-    names(peaks) = c("chrom","start", "end", "name", "score", "strand", 
+    peaks <- read.table(file_path, header = FALSE, skip = skip_lines)
+    peaks <- peaks[,1:10]
+    names(peaks) <- c("chrom","start", "end", "name", "score", "strand",
                         "signalValue", "pValue", "qValue", "peak")
-    
+
     # Validate that all start and end positions are positive values
     if (any(peaks$start < 0) || any(peaks$end < 0)) {
         stop("start and end positions of peaks should all be >= 0.")
     }
-    
+
     # When a dot is used, it has to be changed for an asterisk
     # to be accepted as a GRanges
     if (any(levels(peaks$strand) == ".")) {
         levels(peaks$strand)[levels(peaks$strand) == "."] <- "*"
     }
-    
-    regionResult = NULL;
-    peakResult = NULL;
+
+    regionResult <- NULL
+    peakResult <- NULL
 
     # Create GRanges for the narrow regions when specified
     if (extractRegions) {
-        regionResult <- GRanges(seqnames = as.character(peaks$chrom), 
-                            IRanges(start=(peaks$start + 1L), 
+        regionResult <- GRanges(seqnames = as.character(peaks$chrom),
+                            IRanges(start=(peaks$start + 1L),
                             end=peaks$end),
                             name = as.character(peaks$name),
                             score = as.integer(peaks$score),
@@ -105,10 +105,10 @@ readNarrowPeak<- function(file_path, extractRegions = TRUE,
 
     # Create GRanges for the peaks when specified
     if (extractPeaks) {
-        peakResult <- GRanges(seqnames = as.character(peaks$chrom), 
-                            IRanges(start=(peaks$start + 1L + 
-                            peaks$peak), end=(peaks$start + 1L + 
-                            peaks$peak)), 
+        peakResult <- GRanges(seqnames = as.character(peaks$chrom),
+                            IRanges(start=(peaks$start + 1L +
+                            peaks$peak), end=(peaks$start + 1L +
+                            peaks$peak)),
                             name = as.character(peaks$name),
                             score = as.integer(peaks$score),
                             signalValue = as.numeric(peaks$signalValue),
