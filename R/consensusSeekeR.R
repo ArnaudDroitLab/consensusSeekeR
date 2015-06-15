97a22b4c05a6973adc42644651ef28c5b5d5f8da
adeschen <adeschen@hotmail.com>
2015-06-15 17:17:59 -0400

    Changing dataset names. Updating code using those datasets and documentation pages

--- R/consensusSeekeR.R
+++ R/consensusSeekeR.R
@@ -18,64 +18,74 @@
 #' @keywords package
 NULL
 
-#' Regions of sites with the greatest evidence of transcription factor binding
+#' Genomic regions with the greatest evidence of transcription factor binding
 #' for the FOSL2 transcription factor (for demonstration purpose)
 #'
-#' Regions of sites with the greatest evidence of transcription factor binding
-#' for the
-#' FOSL2 transcription factor (DCC accession: ENCFF000MZT), calculated
-#' using the MACS peak caller (Zhang et al., 2008),
-#' for regions chr1:249120200-249250621 and chr10:1-370100
-#' from
-#' the Encyclopedia of DNA Elements (ENCODE) data (Dunham I et al. 2012).
-#' This dataset is associated to the
-#'
-#' @name Hosa_A549_FOSL2_01_NarrowPeaks
-#' @docType data
-#' @format A \code{GRanges} containing a metadata field called "name"
-#'      which identified the peak associated to the narrow peak region.
-#'      Each \code{GRanges} entry has a
-#'      different peak name.
-#' @usage data(Hosa_A549_FOSL2_01_NarrowPeaks)
-#' @seealso
-#'  \itemize{
-#'    \item \code{\link{Hosa_A549_FOSL2_01_Peaks}} { the associate peaks
-#'                  dataset}
-#'    \item \code{\link{findConsensusPeakRegions}} {for extracting regions
-#'                  sharing the same features in more than one experiment.}
-#'}
-#' @keywords datasets
-NULL
-
-#' Sites with the greatest evidence of transcription factor binding for the
-#' FOSL2 transcription factor (for demonstration purpose)
-#'
-#' Sites with the greatest evidence of transcription factor binding for the
-#' FOSL2 transcription factor (DCC accession: ENCFF000MZT), calculated
-#' using the MACS peak caller (Zhang et al., 2008),
+#' Genomic regions representing the greatest evidence of enrichment for
+#' the FOSL2 transcription factor (DCC accession: ENCFF002CFN)
 #' for regions chr1:249120200-249250621 and chr10:1-370100
 #' from
 #' the Encyclopedia of DNA Elements (ENCODE) data (Dunham I et al. 2012).
 #'
-#' @name Hosa_A549_FOSL2_01_Peaks
+#' @name A549_FOSL2_01_NarrowPeaks_partial
 #' @docType data
-#' @format A \code{GRanges} containing a metadata field called "name"
-#'      which identified the peak. Each \code{GRanges} entry has a
-#'      different peak name.
-#' @usage data(Hosa_A549_FOSL2_01_Peaks)
-#' @seealso
-#'  \itemize{
-#'    \item \code{\link{Hosa_A549_FOSL2_01_NarrowPeaks}} { the associate
-#'                  narrow peaks dataset}
-#'    \item \code{\link{findConsensusPeakRegions}} {for extracting regions
-#'                  sharing the same features in more than one experiment.}
-#' }
+#' @format A \code{GRanges} containing one entry per genomic regions. Each row
+#'  of \code{GRanges} has a name which represent the name of the experiment.
 #' @references
 #'  \itemize{
 #'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of
 #'  DNA elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
-#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et
-#'  al. Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  }
+#' @seealso
+#'  \itemize{
+#'    \item \code{\link{A549_FOSL2_01_Peaks_partial}} { the associate
+#'                  peaks dataset.}
+#'    \item \code{\link{findConsensusPeakRegions}} {for extracting regions
+#'                  sharing the same features in more than one experiment.}
+#' }
+#' @usage data(A549_FOSL2_01_NarrowPeaks_partial)
+#' @keywords datasets
+#' @examples
+#' ## Loading datasets
+#' data(A549_FOSL2_01_NarrowPeaks_partial)
+#' data(A549_FOXA1_01_NarrowPeaks_partial)
+#' data(A549_FOSL2_01_Peaks_partial)
+#' data(A549_FOXA1_01_Peaks_partial)
+#'
+#' ## Assigning experiment name to each row of the dataset
+#' ## NarrowPeak and Peak datasets from the same experiment must
+#' ## have identical name
+#' names(A549_FOXA1_01_Peaks_partial) <- rep("FOXA1_01",
+#'                              length(A549_FOXA1_01_Peaks_partial))
+#' names(A549_FOXA1_01_NarrowPeaks_partial) <- rep("FOXA1_01",
+#'                              length(A549_FOXA1_01_NarrowPeaks_partial))
+#' names(A549_FOSL2_01_Peaks_partial) <-rep("FOSL2_01",
+#'                              length(A549_FOSL2_01_Peaks_partial))
+#' names(A549_FOSL2_01_NarrowPeaks_partial) <- rep("FOSL2_01",
+#'                              length(A549_FOSL2_01_NarrowPeaks_partial))
+#'
+#' ## Calculating consensus regions for chromosome 10
+#' chrList <- Seqinfo("chr10", 135534747, NA)
+#' findConsensusPeakRegions(
+#'      narrowPeaks = c(A549_FOXA1_01_NarrowPeaks_partial, A549_FOSL2_01_NarrowPeaks_partial),
+#'      peaks = c(A549_FOXA1_01_Peaks_partial, A549_FOSL2_01_Peaks_partial),
+#'      chrInfo = chrList, extendingSize = 100, includeAllPeakRegion = FALSE,
+#'      minNbrExp = 2, nbrThreads = 1)
+#'
+NULL
+
+#' TODO Data Set
+#'
+#' TODO
+#'
+#' @name A549_FOSL2_01_Peaks_partial
+#' @docType data
+#' @format A \code{GRanges}
+#' @usage data(A549_FOSL2_01_Peaks_partial)
+#' @references
+#'  \itemize{
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of
+#'  DNA elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
 #'  }
 #' @keywords datasets
 NULL
@@ -84,67 +94,28 @@ NULL
 #'
 #' TODO
 #'
-#' @name Hosa_A549_FOSL2_01_NarrowPeaks_partial
+#' @name A549_FOXA1_01_NarrowPeaks_partial
 #' @docType data
 #' @format A \code{GRanges}
+#' @usage data(A549_FOXA1_01_NarrowPeaks_partial)
 #' @references
 #'  \itemize{
-#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
-#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
-#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
-#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of
+#'  DNA elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
 #'  }
 #' @keywords datasets
 NULL
 
 #' TODO Data Set
 #'
-#' TODO
-#'
-#' @name Hosa_A549_FOSL2_01_Peaks_partial
+#' @name A549_FOXA1_01_Peaks_partial
 #' @docType data
 #' @format A \code{GRanges}
-#' @usage data(Hosa_A549_FOSL2_01_Peaks_partial)
+#' @usage data(A549_FOXA1_01_Peaks_partial)
 #' @references
 #'  \itemize{
-#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
-#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
-#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
-#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
-#'  }
-#' @keywords datasets
-NULL
-
-#' TODO Data Set
-#'
-#' TODO
-#'
-#' @name Hosa_A549_FOXA1_01_NarrowPeaks_partial
-#' @docType data
-#' @format A \code{GRanges}
-#' @usage data(Hosa_A549_FOXA1_01_NarrowPeaks_partial)
-#' @references
-#'  \itemize{
-#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
-#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
-#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
-#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
-#'  }
-#' @keywords datasets
-NULL
-
-#' TODO Data Set
-#'
-#' @name Hosa_A549_FOXA1_01_Peaks_partial
-#' @docType data
-#' @format A \code{GRanges}
-#' @usage data(Hosa_A549_FOXA1_01_Peaks_partial)
-#' @references
-#'  \itemize{
-#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of DNA
-#'  elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
-#'  \item Zhang Y, Liu T, Meyer CA, Eeckhoute J, Johnson DS, Bernstein BE, et al.
-#'  Model-based analysis of ChIP-Seq (MACS). Genome  Biol. 2008, 9(9):R137.
+#'  \item Dunham I, Kundaje A, Aldred SF, et al. An integrated encyclopedia of
+#'  DNA elements in the human genome. Nature. 2012 Sep 6;489(7414):57-74.
 #'  }
 #' @keywords datasets
 NULL
